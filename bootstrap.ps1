269646dde48af045e3bc11e9ae0a70e7aa37bc99
Guenther, Karsten (SD-RM) <Karsten.Guenther@marquardt.com>
2023-01-25 16:46:01 +0100

    load .env

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,15 +4,50 @@ Function Update-Env {
     $Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
 
-# MQ proxy settings
-$ProxyHost = 'osde01proxy02.marquardt.de:8080'
-$Env:HTTP_PROXY = "http://$ProxyHost"
-$Env:HTTPS_PROXY = $Env:HTTP_PROXY
-$Env:NO_PROXY = "localhost,.marquardt.de,.marquardt.com"
-$WebProxy = New-Object System.Net.WebProxy($Env:HTTP_PROXY, $true, ($Env:NO_PROXY).split(','))
-[net.webrequest]::defaultwebproxy = $WebProxy
-[net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+Function Invoke-CommandLine {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$CommandLine,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [bool]$StopAtError = $true,
+        [Parameter(Mandatory = $false, Position = 2)]
+        [bool]$Silent = $false
+    )
+    if (-Not$Silent) {
+        Write-Host "Executing: $CommandLine"
+    }
+    Invoke-Expression $CommandLine
+    if ($LASTEXITCODE -ne 0) {
+        if ($StopAtError) {
+            Write-Error "Command line call `"$CommandLine`" failed with exit code $LASTEXITCODE"
+            exit 1
+        }
+        else {
+            if (-Not$Silent) {
+                Write-Host "Command line call `"$CommandLine`" failed with exit code $LASTEXITCODE, continuing ..."
+            }
+        }
+    }
+}
 
+if (Test-Path -Path '.env') {
+    # load environment properties
+    $envProps = ConvertFrom-StringData (Get-Content '.env' -raw)
+}
+
+if ($envProps.'HTTP_PROXY') {
+    $Env:HTTP_PROXY = $envProps.'HTTP_PROXY'
+    $Env:HTTPS_PROXY = $Env:HTTP_PROXY
+    if ($envProps.'NO_PROXY') {
+        $Env:NO_PROXY = $envProps.'NO_PROXY'
+        $WebProxy = New-Object System.Net.WebProxy($Env:HTTP_PROXY, $true, ($Env:NO_PROXY).split(','))
+    }
+    else {
+        $WebProxy = New-Object System.Net.WebProxy($Env:HTTP_PROXY, $true)
+    }
+    [net.webrequest]::defaultwebproxy = $WebProxy
+    [net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+}
 
 if (Test-Path -Path 'scoopfile.json') {
     # Initial Scoop installation
@@ -22,32 +57,32 @@ if (Test-Path -Path 'scoopfile.json') {
     }
 
     # Some old tweak to get 7zip installed correctly
-    scoop config use_lessmsi $true
+    Invoke-CommandLine -CommandLine "scoop config use_lessmsi $true"
 
     # avoid deadlocks while updating scoop buckets
-    scoop config autostash_on_conflict $true
+    Invoke-CommandLine -CommandLine "scoop config autostash_on_conflict $true"
 
     # install needed tools
-    scoop update
-    scoop import scoopfile.json
+    Invoke-CommandLine -CommandLine "scoop update"
+    Invoke-CommandLine -CommandLine "scoop import scoopfile.json"
     Update-Env
 }
 
 if ((Test-Path -Path 'requirements.txt') -or (Test-Path -Path 'Pipfile')) {
     # Prepare python environment
-    python -m pip install pipenv
+    Invoke-CommandLine -CommandLine "python -m pip install pipenv"
     Update-Env
     if ($clean) {
         # Start with a fresh virtual environment
-        python -m pipenv --rm
+        Invoke-CommandLine -CommandLine "python -m pipenv --rm"
     }
     if (-Not (Test-Path -Path '.venv')) {
         New-Item -ItemType Directory '.venv'
     }
     if (Test-Path -Path 'requirements.txt') {
-        python -m pipenv install --requirements requirements.txt
+        Invoke-CommandLine -CommandLine "python -m pipenv install --requirements requirements.txt"
     }
     else {
-        python -m pipenv install
+        Invoke-CommandLine -CommandLine "python -m pipenv install"
     }
 }
